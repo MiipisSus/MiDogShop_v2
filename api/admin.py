d69633cb5d1b4ae98d00a13d2fbfce27a606a473
MiipisSus <kokurui0113@gmail.com>
2025-05-23 18:57:39 +0800

    feat: Register some models in admin site.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Category, Product, ProductVariant, ProductValue, ProductOption, VariantValueMap
+
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductVariant)
+admin.site.register(ProductValue)
+admin.site.register(ProductOption)
+admin.site.register(VariantValueMap)

cf9c378b229e1813fb621de4594077cfd6ee84e7
MiipisSus <kokurui0113@gmail.com>
2025-05-27 17:08:44 +0800

    feat: Add `IsAdminUserOrOwner` permission

--- api/permissions.py
+++ api/permissions.py
@@ -2,18 +2,33 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 from api.models import CustomerAddressHome, Order, User
 
-class IsOwnerOrReadOnly(BasePermission):
+
+def check_object_owner(obj, user):
+    if isinstance(obj, CustomerAddressHome) or isinstance(obj, Order):
+        return obj.customer == user
+    elif isinstance(obj, User):
+        return obj == user
+    else:
+        return False
+
+
+class IsOwner(BasePermission):
     """
     只允許物件的擁有者編輯它
     """
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-          
         # Add any model check if needed
-        if isinstance(obj, CustomerAddressHome) or isinstance(obj, Order):
-          return obj.customer == request.user
-        elif isinstance(obj, User):
-          return obj == request.user
+        return check_object_owner(obj, request.user)
+
+
+class IsAdminUserOrOwner(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return bool(request.user and request.user.is_authenticated)
+        # Create methods only available to staff. If the user is not staff, they should use /me endpoint
+        elif request.method == 'POST':
+            return bool(request.user and request.user.is_staff)
         else:
-          return False
+            return True
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user and (request.user.is_staff or check_object_owner(obj, request.user)))

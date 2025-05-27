675d3ce365096364b8b96bc230d7d5630fcf9726
MiipisSus <kokurui0113@gmail.com>
2025-05-27 17:10:01 +0800

    fix: Fix some authorization logic and bugs in UserViewSet and CustomerAddressHomeViewSet.

--- api/serializers/users.py
+++ api/serializers/users.py
@@ -1,36 +1,38 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, ValidationError
 from django.contrib.auth.models import Group
 
 from api.models import User, CustomerAddressHome
 
 
-class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    exclude = ['user_permissions', 'groups', 'is_superuser', 'is_staff', 'is_active']
-    read_only_fields = ['last_login', 'date_joined', 'groups', 'user_permissions']
-    extra_kwargs = {
-      'password': {'write_only': True}
-    }
-  
-  def validate(self, data):
-        request_user = self.context['request'].user
-        instance_user = self.instance
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['user_permissions', 'groups',
+                   'is_superuser', 'is_staff', 'is_active']
+        read_only_fields = ['last_login',
+                            'date_joined', 'groups', 'user_permissions']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
-        if instance_user and request_user != instance_user and not request_user.is_staff:
-            raise serializers.ValidationError({"detail": "你無權修改其他使用者的資料。"})
-        
-        return data
-      
-  def create(self, validated_data):
-    # encode password
-    user = User.objects.create_user(**validated_data)
-    customer_group = Group.objects.get(name='Customer')
-    user.groups.add(customer_group)
-    return user
-  
+    def create(self, validated_data):
+        # encode password
+        instance: User = super().create(validated_data)
+        instance.set_password(instance.password)
+        instance.save()
+        customer_group = Group.objects.get(name='Customer')
+        instance.groups.add(customer_group)
+        return instance
 
-class CustomerAddressHomeSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = CustomerAddressHome
-    fields = '__all__'
+    def update(self, instance, validated_data):
+        # encode password
+        instance: User = super().update(instance, validated_data)
+        instance.set_password(instance.password)
+        instance.save()
+        return instance
+
+
+class CustomerAddressHomeSerializer(ModelSerializer):
+    class Meta:
+        model = CustomerAddressHome
+        fields = '__all__'
--- api/views/users.py
+++ api/views/users.py
@@ -1,28 +1,67 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 
 from api.models import User, CustomerAddressHome
 from api.serializers.users import UserSerializer, CustomerAddressHomeSerializer
-from api.permissions import IsOwnerOrReadOnly
+from api.permissions import IsAdminUserOrOwner
+
 
 class UserViewSet(ModelViewSet):
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
-  permission_classes = [IsOwnerOrReadOnly]
-  
-  def get_queryset(self):
-    if self.request.user.is_staff:
-      return User.objects.all()
-    return super().get_queryset().filter(groups__name='Customer')
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUserOrOwner]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return User.objects.all()
+        return super().get_queryset().filter(groups__name='Customer')
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return []
+        elif self.action == 'retrieve':
+            return [IsAuthenticated()]
+        return super().get_permissions()
+
+    @action(detail=False, methods=['get', 'patch', 'put', 'delete'],
+            url_path='me', permission_classes=[IsAuthenticated])
+    def me(self, request):
+        user = request.user
+        self.kwargs[self.lookup_field] = user.pk
+
+        if request.method == 'GET':
+            return self.retrieve(request)
+        elif request.method in ['PATCH', 'PUT']:
+            return self.partial_update(request) if request.method == 'PATCH' else self.update(request)
+        elif request.method == 'DELETE':
+            return self.destroy(request)
 
 
 class CustomerAddressHomeViewSet(ModelViewSet):
-  queryset = CustomerAddressHome.objects.all()
-  serializer_class = CustomerAddressHomeSerializer
-  permission_classes = [IsAuthenticated]
-  
-  def get_queryset(self):
-    if self.request.user.is_staff:
-      return CustomerAddressHome.objects.all()
-    return super().get_queryset().filter(customer=self.request.user)
+    queryset = CustomerAddressHome.objects.all()
+    serializer_class = CustomerAddressHomeSerializer
+    permission_classes = [IsAdminUserOrOwner]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return CustomerAddressHome.objects.all()
+        return super().get_queryset().filter(customer=self.request.user)
+    
+    @action(detail=False, methods=['get', 'post'],
+            url_path='me', permission_classes=[IsAuthenticated])
+    def me(self, request):
+        user = request.user
+        self.kwargs[self.lookup_field] = user.pk
+        
+        if request.method == 'GET':
+            return self.list(request)
+        elif request.method == 'POST':
+            request.data['customer'] = user.pk
+            return self.create(request)
+        
+    def update(self, request, *args, **kwargs):
+        # prevent the customer_id to be changed
+        request.data['customer'] = self.request.user.pk
+        return super().update(request, *args, **kwargs)
+    

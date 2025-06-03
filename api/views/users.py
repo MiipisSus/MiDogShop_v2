a0cf5966a4b2edc110e97b3d961bb836a79311e0
MiipisSus <kokurui0113@gmail.com>
2025-06-03 13:59:02 +0800

    feat: Redesign order and user viewsets permissions.

--- api/views/users.py
+++ api/views/users.py
@@ -1,11 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, SAFE_METHODS
 from rest_framework.decorators import action
 from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from api.models import User, CustomerAddressHome
 from api.serializers.users import UserSerializer, CustomerAddressHomeSerializer
-from api.permissions import IsAdminUserOrOwner
 from api.common import PERMMISIONS_DOCS
 
 
@@ -13,14 +12,14 @@ from api.common import PERMMISIONS_DOCS
     list=extend_schema(summary=f"取得使用者清單 {PERMMISIONS_DOCS.IsAuthenticated}"),
     create=extend_schema(summary=f"新增使用者 {PERMMISIONS_DOCS.NoAuth}"),
     retrieve=extend_schema(summary=f"取得使用者資料 {PERMMISIONS_DOCS.IsAuthenticated}"),
-    update=extend_schema(summary=f"更新使用者資料 {PERMMISIONS_DOCS.IsAdminUserOrOwner}"),
-    partial_update=extend_schema(summary=f"更新使用者資料 {PERMMISIONS_DOCS.IsAdminUserOrOwner}"),
-    destroy=extend_schema(summary=f"刪除使用者資料 {PERMMISIONS_DOCS.IsAdminUserOrOwner}"),
+    update=extend_schema(summary=f"更新使用者資料 {PERMMISIONS_DOCS.IsAdminUser}"),
+    partial_update=extend_schema(summary=f"更新使用者資料 {PERMMISIONS_DOCS.IsAdminUser}"),
+    destroy=extend_schema(summary=f"刪除使用者資料 {PERMMISIONS_DOCS.IsAdminUser}"),
 )
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUserOrOwner]
+    permission_classes = [IsAdminUser]
 
     def get_queryset(self):
         if self.request.user.is_staff:
@@ -30,7 +29,7 @@ class UserViewSet(ModelViewSet):
     def get_permissions(self):
         if self.action == 'create':
             return []
-        elif self.action == 'retrieve':
+        elif self.request.method in SAFE_METHODS:
             return [IsAuthenticated()]
         return super().get_permissions()
 
@@ -64,36 +63,20 @@ class UserViewSet(ModelViewSet):
 
 @extend_schema_view(
     list=extend_schema(summary=f"取得使用者地址清單 {PERMMISIONS_DOCS.IsAuthenticated}"),
-    create=extend_schema(summary=f"新增使用者地址 {PERMMISIONS_DOCS.IsAdminUser}"),
+    create=extend_schema(summary=f"新增使用者地址 {PERMMISIONS_DOCS.IsAuthenticated}"),
     retrieve=extend_schema(summary=f"取得使用者地址資料 {PERMMISIONS_DOCS.IsAuthenticated}"),
-    update=extend_schema(summary=f"更新使用者地址資料 {PERMMISIONS_DOCS.IsAdminUserOrOwner}"),
-    partial_update=extend_schema(summary=f"更新使用者地址資料 {PERMMISIONS_DOCS.IsAdminUserOrOwner}"),
-    destroy=extend_schema(summary=f"刪除使用者地址資料 {PERMMISIONS_DOCS.IsAdminUserOrOwner}"),
+    update=extend_schema(summary=f"更新使用者地址資料 {PERMMISIONS_DOCS.IsAuthenticated}"),
+    partial_update=extend_schema(summary=f"更新使用者地址資料 {PERMMISIONS_DOCS.IsAuthenticated}"),
+    destroy=extend_schema(summary=f"刪除使用者地址資料 {PERMMISIONS_DOCS.IsAuthenticated}"),
 )
 class CustomerAddressHomeViewSet(ModelViewSet):
     queryset = CustomerAddressHome.objects.all()
     serializer_class = CustomerAddressHomeSerializer
-    permission_classes = [IsAdminUserOrOwner]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         if self.request.user.is_staff:
             return CustomerAddressHome.objects.all()
         return super().get_queryset().filter(customer=self.request.user)
-    
-    @action(detail=False, methods=['get', 'post'],
-            url_path='me', permission_classes=[IsAuthenticated])
-    def me(self, request):
-        user = request.user
-        self.kwargs[self.lookup_field] = user.pk
-        
-        if request.method == 'GET':
-            return self.list(request)
-        elif request.method == 'POST':
-            request.data['customer'] = user.pk
-            return self.create(request)
-        
-    def update(self, request, *args, **kwargs):
-        # prevent the customer_id to be changed
-        request.data['customer'] = self.request.user.pk
-        return super().update(request, *args, **kwargs)
+
     

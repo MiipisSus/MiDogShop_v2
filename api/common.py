f0d72369e8a5a24447e0da8962f997c4f7c73f80
MiipisSus <kokurui0113@gmail.com>
2025-06-03 13:57:14 +0800

    chore: Delete uneeded permission class.

--- api/common.py
+++ api/common.py
@@ -2,6 +2,5 @@ class PERMMISIONS_DOCS:
     NoAuth = '（無需權限）'
     IsAuthenticated = '（需要權限）'
     IsAdminUser = '（需要 Admin 權限）'
-    IsAdminUserOrOwner = '（需要 Admin 權限或是擁有者）'
     IsManager = '（需要 Manager 權限或是擁有者）'
     IsStaff = '（需要 Staff 權限或是擁有者）'
--- api/permissions.py
+++ api/permissions.py
@@ -1,35 +1,8 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 from api.models import CustomerAddressHome, Order, User
-
-
-def check_object_owner(obj, user):
-    if isinstance(obj, CustomerAddressHome) or isinstance(obj, Order):
-        return obj.customer == user
-    elif isinstance(obj, User):
-        return obj == user
-    else:
-        return False
-
-
-class IsAdminUserOrOwner(BasePermission):
-    """
-    GET: Any authenticated user\n
-    POST: Only staff user\n
-    PATCH, PUT, DELETE: Only staff user or the owner
-    """
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return bool(request.user and request.user.is_authenticated)
-        # Create methods only available to staff. If the user is not staff, they should use /me endpoint
-        elif request.method == 'POST':
-            return bool(request.user and request.user.is_staff)
-        else:
-            return True
-    def has_object_permission(self, request, view, obj):
-        return bool(request.user and (request.user.is_staff or check_object_owner(obj, request.user)))
-
-
+    
+    
 class IsStaff(BasePermission):
     def has_permission(self, request, view):
         user = request.user
